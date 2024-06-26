41db57bb3af425f402017bba73075cb6ea1971aa
SimilArticles <stefano.coppola87@gmail.com>
2024-06-26 13:53:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ async function findRelatedWorks() {
     try {
         // Step 2: Query OpenAlex API with DOI
         const response = await fetch(`https://api.openalex.org/works?filter=doi:${doi}`);
+        resultsDiv.innerHTML = <h3>https://api.openalex.org/works?filter=doi:${doi}</h3>
         const data = await response.json();
 
         if (data.results && data.results.length > 0) {
